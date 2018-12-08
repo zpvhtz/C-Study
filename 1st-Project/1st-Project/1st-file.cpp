d65142b2a8d594eb54c5f0fe553ae8f826edd2a3
HeoBayMau <hieu.trung.030197@gmail.com>
2018-12-08 23:00:28 +0700

    Update

--- 1st-Project/1st-Project/1st-file.cpp
+++ 1st-Project/1st-Project/1st-file.cpp
@@ -15,6 +15,42 @@ int main()
 	printf("\n\nBài 02:\n");
 	printf("STDIO stands for StandarD Input Output");
 
+	//Bài 03
+	printf("\n\nBài 03:\n");
+	printf(
+		" _______  _______  ______   ___   _______ \n"
+		"|       ||       ||      | |   | |       |\n"
+		"|  _____|| _     ||  _    ||   | |   _   |\n"
+		"| |_____   |   |  | | |   ||   | |  | |  |\n"
+		"|_____  |  |   |  | |_|   ||   | |  |_|  |\n"
+		"______| |  |   |  |       ||   | |       |\n"
+		"|_______|  |___|  |______| |___| |_______|\n"
+	);
+
+	//Bài 04
+	printf("\n\nBài 04:\n");
+	printf(
+		"Microsoft DirectX is a collection of application programming interfaces (APIs) for handling tasks related to multimedia,\nespecially game programming and video, on Microsoft platforms."
+		"Originally, the names of these APIs all began with Direct, such as\n\n"
+		"\t- Direct3D\n"
+		"\t- DirectDraw\n"
+		"\t- DirectDraw\n"
+		"\t- DirectPlay\n"
+		"\t- DirectSound\n"
+		"\t- And so forth.\n\n"
+		"The name DirectX was coined as shorthand term for all of these APIs (the X standing in for the particular API names) and\nsoon became the name of the collection."
+	);
+
+	//Bài 05
+	printf("\n\nBài 05:\n");
+	printf(
+		"In C++, we have some special characters:\n"
+		"\t- // or /* */ for the comment.\n"
+		"\t- \\t for tab.\n"
+		"\t- \\n for new line.\n"
+		"\t- \" and \" to mark it's a string."
+	);
+
 	while (true);
 	return 0;
 }
