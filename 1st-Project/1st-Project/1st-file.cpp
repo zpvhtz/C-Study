6e078aa9aeeb5c94c29d0151c10069216cb7b5a1
HeoBayMau <hieu.trung.030197@gmail.com>
2018-12-08 12:47:02 +0700

    ...

---
+++ 1st-Project/1st-Project/1st-file.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	//printf, scanf
+	//\n = newline, \t = tab
+	printf("Hello \"World\n");
+	printf("Hello \\World\n");
+
+	//Bài 01
+	printf("\n\nBài 01:\n");
+	printf("www.stdio.vn");
+
+	//Bài 02
+	printf("\n\nBài 02:\n");
+	printf("STDIO stands for StandarD Input Output");
+
+	while (true);
+	return 0;
+}
