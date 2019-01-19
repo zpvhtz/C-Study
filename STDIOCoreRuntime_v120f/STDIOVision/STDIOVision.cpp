c00b03e9a64c2ea55ee947b484a4dd184715dc88
HeoBayMau <hieu.trung.030197@gmail.com>
2019-01-19 09:44:40 +0700

    Update

--- STDIOCoreRuntime_v120f/STDIOVision/STDIOVision.cpp
+++ STDIOCoreRuntime_v120f/STDIOVision/STDIOVision.cpp
@@ -10,7 +10,8 @@ void STDIOVisionMinMaxRange(double* min, double* max, double* init)
 void STDIOVisionProcess(int width, int height, int byte_per_pixel, byte*& data, double value)
 {
 	printf("STDIO DEBUG");
-	GrayscaleImage(width, height, byte_per_pixel, data, value);
+	//GrayscaleImage(width, height, byte_per_pixel, data, value);
+	IncreaseBrightnessArtOfImage(width, height, byte_per_pixel, data, value);
 }
 
 void GrayscaleImage(int width, int height, int byte_per_pixel, byte*& data, double value)
@@ -19,6 +20,72 @@ void GrayscaleImage(int width, int height, int byte_per_pixel, byte*& data, doub
 
 void IncreaseBrightnessOfImage(int width, int height, int byte_per_pixel, byte*& data, double value)
 {
+	int X = 0, Y = 1, W = 200, H = 150;
+
+	// Rectangle: X, Y, W, H
+	int rect[4] = { 100, 100, 200, 150 };
+
+	// Recompute rectangle
+	if (rect[X] < 0)
+		rect[X] = 0;
+	if (rect[X] + rect[W] > width)
+		rect[W] = width - rect[X];
+	if (rect[Y] < 0)
+		rect[Y] = 0;
+	if (rect[Y] + rect[H] > height)
+		rect[H] = height - rect[Y];
+
+	int index = 0;
+	int component = 0;
+
+	for (int r = rect[Y]; r < rect[Y] + rect[H]; r++)
+	{
+		for (int c = rect[X]; c < rect[X] + rect[W]; c++)
+		{
+			index = (r * width + c) * byte_per_pixel;
+
+			component = *(data + index + 0) * value;
+			*(data + index + 0) = component > 255 ? 255 : component;
+
+			component = *(data + index + 1) * value;
+			*(data + index + 1) = component > 255 ? 255 : component;
+
+			component = *(data + index + 2) * value;
+			*(data + index + 2) = component > 255 ? 255 : component;
+		}
+	}
+}
+
+void IncreaseBrightnessArtOfImage(int width, int height, int byte_per_pixel, byte* &data, double value)
+{
+	double bright_1 = value;
+	int resolution = width * height;
+	for (int row = 0; row < height; row++)
+	{
+		for (int col = 0; col < width; col++)
+		{
+			int index = (row * width + col) * byte_per_pixel;
+
+			if (col < width / 8)
+			{
+				*(data + index + 1) = *(data + index + 1) * value > 255 ? 255 : *(data + index + 1) * value;
+				*(data + index + 2) = *(data + index + 2) * value > 255 ? 255 : *(data + index + 2) * value;
+				*(data + index + 3) = *(data + index + 3) * value > 255 ? 255 : *(data + index + 3) * value;
+			}
+			else if (col < width / 4)
+			{
+				*(data + index + 1) = *(data + index + 1) * (value - 0.2) > 255 ? 255 : *(data + index + 1) * (value - 0.2);
+				*(data + index + 2) = *(data + index + 2) * (value - 0.2) > 255 ? 255 : *(data + index + 2) * (value - 0.2);
+				*(data + index + 3) = *(data + index + 3) * (value - 0.2) > 255 ? 255 : *(data + index + 3) * (value - 0.2);
+			}
+			else if (col < width / 2)
+			{
+				*(data + index + 1) = *(data + index + 1) * (value - 0.4) > 255 ? 255 : *(data + index + 1) * (value - 0.4);
+				*(data + index + 2) = *(data + index + 2) * (value - 0.4) > 255 ? 255 : *(data + index + 2) * (value - 0.4);
+				*(data + index + 3) = *(data + index + 3) * (value - 0.4) > 255 ? 255 : *(data + index + 3) * (value - 0.4);
+			}
+		}
+	}
 }
 
 void IncreaseContrastOfImage(int width, int height, int byte_per_pixel, byte*& data, double value)
@@ -27,6 +94,49 @@ void IncreaseContrastOfImage(int width, int height, int byte_per_pixel, byte*& d
 
 void BoundingBoxImage(int width, int height, int byte_per_pixel, byte*& data, double value)
 {
+	int borderWidth = (int)value;
+
+	byte color[3] = { 0, 136, 255 };
+
+	int index = 0;
+
+	// ooooo
+	// x   x
+	// ooooo
+	for (int r = 0; r < borderWidth; r++)
+	{
+		for (int c = 0; c < width; c++)
+		{
+			index = (r * width + c) * byte_per_pixel;
+			*(data + index + 0) = color[0];
+			*(data + index + 1) = color[1];
+			*(data + index + 2) = color[2];
+
+			index = ((height - r - 1) * width + c) * byte_per_pixel;
+			*(data + index + 0) = color[0];
+			*(data + index + 1) = color[1];
+			*(data + index + 2) = color[2];
+		}
+	}
+
+	// oxxxo
+	// o   o
+	// oxxxo
+	for (int r = 0; r < height; r++)
+	{
+		for (int c = 0; c < borderWidth; c++)
+		{
+			index = (r * width + c) * byte_per_pixel;
+			*(data + index + 0) = color[0];
+			*(data + index + 1) = color[1];
+			*(data + index + 2) = color[2];
+
+			index = (r * width + (width - c - 1)) * byte_per_pixel;
+			*(data + index + 0) = color[0];
+			*(data + index + 1) = color[1];
+			*(data + index + 2) = color[2];
+		}
+	}
 }
 
 void DecreaseOpacityOfHalfImage(int width, int height, int byte_per_pixel, byte*& data, double value)
